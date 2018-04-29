abfdf50aa60ff9760f9f631f2a220a588c74738e
Abhi <despotar1712@gmail.com>
2018-04-30 00:18:51 +0200

    Berechnungsmethode eingefügt zum testen ob es funktioniert. *Für jeden Punkt Distanz berechnet und Liste nach dieser Distanz sortiert *Methode implementiert die eine neue Liste zurückgibt mit den Stationen innerhalb der Distanz

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
@@ -10,13 +11,39 @@ public class Main {
 
         ArrayList<Station> stations = getStationsFromFile();
 
+        stations.sort(Comparator.comparingDouble(Station::getDistance)); //SORT LIST
+
+        /*
         for(Station s : stations){
-            System.out.println(s.toString());
+            System.out.println(s.getDistanceFromZero() + "   " + s.toString());
+        }*/
+
+        ArrayList<Station> stationsFromCoord = getStationsFromCoord(stations, 4240.270712914558,4492.579044115013, 200.0);
+        for(Station s:stationsFromCoord){
+            System.out.println(s.getDistance() + "   " + s.toString());
         }
+
+
     }
 
-    //TEST KOMMENTAR FÜR GIT WIEDER LÖSCHEN
-    //TEST 2 FÜR GIT
+    private static ArrayList<Station> getStationsFromCoord(ArrayList<Station> stations, double x, double y, double radius){
+        double distance = Math.sqrt(Math.pow(x,2) + Math.pow(y,2));//Distance den gesuchten Koordinaten
+        double low = distance - radius; // Untergrenze
+        double high = distance + radius; //Obergrenze
+        ArrayList<Station> tempList = new ArrayList<>();
+
+        for(Station s : stations){
+
+            if(s.getDistance() >= low && s.getDistance() <= high){ //Verlgeich ob die distnz in der Grenze liegt
+                tempList.add(s);
+            }
+        }
+
+        return  tempList; //Return wert eine arrayliste mit den Stationen die das Kriterium erfüllen
+
+    }
+
+
     private static ArrayList<Station> getStationsFromFile(){
 
         ArrayList<Station> stations = new ArrayList<>();
--- src/Station.java
+++ src/Station.java
@@ -2,11 +2,14 @@ public class Station {
     private double x;
     private double y;
     private String type;
+    private double distance; //Varibale in die die Distance abgespeichert wird
+
 
     public Station(double x, double y, String type) {
         this.x = x;
         this.y = y;
         this.type = type;
+        distance = Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
     }
 
     public double getX() {
@@ -19,6 +22,11 @@ public class Station {
 
     public String getType() {
         return type;
+    }
+
+    //Gibt die Distanz jeder Station vom NUll punkt an (nur ein Versuch)
+    public double getDistance(){
+        return distance;
     }
 
     @Override
